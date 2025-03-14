8086aa15f73d5643a71027bc67dd1913954aba2b
Simon Jones <si@thecsharper.com>
2025-03-14 06:35:44 +0000

    Added examples

--- HumanizerDemo/Program.cs
+++ HumanizerDemo/Program.cs
@@ -1,11 +1,23 @@
 ﻿using Humanizer;
 
+// From: https://github.com/Humanizr/Humanizer
+
 Write("Sentence casing".Transform(To.LowerCase));
 Write("Sentence casing".Transform(To.SentenceCase));
 Write("Sentence casing".Transform(To.TitleCase));
 Write("Sentence casing".Transform(To.UpperCase));
 
 Write("Sentence casing".Humanize(LetterCasing.Title));
+
+Write("Long text to truncate".Truncate(10, "---", Truncator.FixedLength));
+
+DateTime.UtcNow.AddHours(-30).Humanize();
+DateTime.UtcNow.AddHours(-2).Humanize();
+
+DateTime.UtcNow.AddHours(30).Humanize();
+DateTime.UtcNow.AddHours(2).Humanize();
+
+DateTimeOffset.UtcNow.AddHours(1).Humanize();
 
 static void Write(string text)
 {
